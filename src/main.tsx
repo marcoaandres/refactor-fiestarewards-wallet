baf47ce560a83efe439b7921111ac8c61c5afcb2
Marco Andres <marcoa@gluo.mx>
2023-11-20 15:19:07 -0600

    Config for gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+// import { BrowserRouter } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
 
 import theme from './theme'
 import App from './App.tsx'
 import {Fonts} from './theme/Fonts.tsx'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       {/* use react router */}
-      <BrowserRouter>
-        <Fonts/>
-        <App />
-      </BrowserRouter>
+      {/* <BrowserRouter> */}
+        <HashRouter>
+          <Fonts/>
+          <App />
+        </HashRouter>
+      {/* </BrowserRouter> */}
     </ChakraProvider>
   </React.StrictMode>,
 )
