f7849161a112bec46185ba6b2225c52291a064e5
Marco Andres <marcoa@gluo.mx>
2024-08-30 15:04:57 -0600

    Add. Show and close sidebar menu

--- src/ui/WalletSidebar.tsx
+++ src/ui/WalletSidebar.tsx
@@ -1,8 +1,8 @@
-import { useState } from "react"
 import { Link as ReactLink } from "react-router-dom"
 import { Box, Link} from "@chakra-ui/react"
 import { ChevronDownIcon, ChevronUpIcon } from "@chakra-ui/icons"
 import { useTranslation } from "react-i18next"
+import { useComponentVisible } from "../hooks"
 
 
 type props = {
@@ -11,15 +11,16 @@ type props = {
 
 export const WalletSidebar = ({ programs }:props) => {
 
-    const [showMenu, setShowMenu] = useState(false)
     const { i18n } = useTranslation()
     const currentLanguaje = i18n.resolvedLanguage 
+    const { ref, isComponentVisible: showMenu, setIsComponentVisible: setShowMenu } = useComponentVisible(false)
 
     const onToggleMenu = () => {
         setShowMenu(!showMenu)
     }
+    const handlerCloseMenu = (): void => setShowMenu(false) 
   return (
-    <Box zIndex="docked" bgColor="primary.10" pt={{ base:"25", md: "32px" }} h={{ base:"auto", md: "100%" }} w={{base:"100%", md: "auto"}} position={{base: "fixed", md: "initial" }}>
+    <Box zIndex="docked" bgColor="primary.10" pt={{ base:"25", md: "32px" }} h={{ base:"auto", md: "100%" }} w={{base:"100%", md: "auto"}} position={{base: "fixed", md: "initial" }} ref={ref}>
         <Box visibility={{ base: 'visible', md: 'hidden' }} color="white" p="16px 24px" onClick={onToggleMenu}>
            <Box display={{base: 'block', md: 'none'}}>
                 <span>Mis membresías</span> {showMenu ? <ChevronDownIcon boxSize={6}/> : <ChevronUpIcon boxSize={6}/>}
@@ -29,7 +30,7 @@ export const WalletSidebar = ({ programs }:props) => {
             {
                 programs.map((program)=> (
                     <Box key={program}>
-                        <Link as={ReactLink} to={`/${currentLanguaje}/my-memberships?program=${program}`} p="16px 32px" display="block" variant="sidebar">{program === 'FR' ? 'Fiesta Rewards' : 'Apreciare'}</Link>
+                        <Link as={ReactLink} to={`/${currentLanguaje}/my-memberships?program=${program}`} onClick={handlerCloseMenu} p="16px 32px" display="block" variant="sidebar">{program === 'FR' ? 'Fiesta Rewards' : 'Apreciare'}</Link>
                     </Box>
                 ))
             }
